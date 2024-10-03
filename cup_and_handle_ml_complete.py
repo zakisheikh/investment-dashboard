f797ecc1c03107b9deb5bd35739b504069b24d91
Zaki Sheikh <zshake21@gmail.com>
2024-10-02 17:37:57 -0700

    Update cup_and_handle_ml_complete.py

--- cup_and_handle_ml_complete.py
+++ cup_and_handle_ml_complete.py
@@ -10,7 +10,7 @@ from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 import mplfinance as mpf
-from datetime as datetime, timedelta
+from datetime import datetime, timedelta
 import os
 
 # Suppress warnings
