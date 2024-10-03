6beeecabd44c72a3c104ba17d7e272c9d9dcf3c8
Zaki Sheikh <zshake21@gmail.com>
2024-10-02 19:36:56 -0700

    Update cup_recognition.py

--- cup_recognition.py
+++ cup_recognition.py
@@ -209,8 +209,9 @@ if len(pattern_indices) > 0:
     
     # st.write(f"Last detected cup and handle pattern from {start_date_formatted} to {end_date_formatted}")
     # st.write(f"Price range: {price_min:.2f} to {price_max:.2f}")
-    st.write(f"🎯 Look at that! The last cup and handle pattern emerged between {start_date_formatted} and {end_date_formatted}. 
+    st.write(f"🎯 Look at that! The last cup and handle pattern emerged between {start_date_formatted} and {end_date_formatted}.")
     st.write(f"Ready for the numbers? 📈 Price danced from ${price_min:.2f} to ${price_max:.2f}.")
+
 
     
     candlestick_data = window[['Open', 'High', 'Low', 'Close']].copy()
