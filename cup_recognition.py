3988c67ff29c3755f5a9890380f5c9915b1c51ee
Zaki Sheikh <zshake21@gmail.com>
2024-10-02 19:49:52 -0700

    Update cup_recognition.py

--- cup_recognition.py
+++ cup_recognition.py
@@ -210,7 +210,7 @@ if len(pattern_indices) > 0:
     # st.write(f"Last detected cup and handle pattern from {start_date_formatted} to {end_date_formatted}")
     # st.write(f"Price range: {price_min:.2f} to {price_max:.2f}")
     st.write(f"🎯 Jackpot! The last cup and handle pattern emerged between {start_date_formatted} and {end_date_formatted}.")
-    st.markdown(f"📈 **Ready for the numbers?** Price danced from **${price_min:.2f}** to **${price_max:.2f}**.")
+    st.markdown(f"📈 **Ready for the numbers?** Price danced from ${price_min:.2f} to ${price_max:.2f}.")
 
 
 
